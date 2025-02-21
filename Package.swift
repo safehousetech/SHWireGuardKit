4056662f4e71ec57a57167f59d64aba295e93562
AbhishekChoudhary4 <abhishek@safehousetech.com>
2025-02-21 11:58:34 +0530

    Changed file path for WireguardkitC file issue

--- Package.swift
+++ Package.swift
@@ -8,21 +8,22 @@
 import PackageDescription
 
 let package = Package(
-	name: "SHWireGuardKit",
-	platforms: [
-		.macOS(.v10_15),
-		.iOS(.v12)
-	],
-	products: [
-		.library(name: "SHWireGuardKit", targets: ["SHWireGuardKit"])
-	],
-	targets: [
-		.binaryTarget(
-			name: "SHWireGuardKit",
-			url: "https://github.com/safehousetech/SHWireGuardKit/releases/download/1.0.0/Safehouse-bodyguard-swift6-update.zip",
-			checksum: "a779750fd65b74e466b6a6d98c30029632754f9a6f942e32993d331d5037f0f1"
-		)
-	]
+    name: "SHWireGuardKit",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v12)
+    ],
+    products: [
+        .library(name: "SHWireGuardKit", targets: ["SHWireGuardKit"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "SHWireGuardKit",
+            url: "https://github.com/AbhishekChoudhary4/Safehouse-bodyguard-swift6-update/releases/download/1.0.0/Safehouse-bodyguard-swift6-update.zip",
+            checksum: "a779750fd65b74e466b6a6d98c30029632754f9a6f942e32993d331d5037f0f1"
+        )
+    ]
 )
 
 //https://github.com/safehousetech/SHWireGuardKit/releases/download/1.0.0/SHWireGuardKit.xcframework.zip
+
