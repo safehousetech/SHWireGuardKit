3d23d2c418143c35188a163cad0c39343fe772b0
AbhishekChoudhary4 <abhishek@safehousetech.com>
2025-03-05 17:10:41 +0530

    Revert "[ADD]:-Changes deploying for testing purpose will fallback if needed"
    This reverts commit 0ed54bd226b637786e4cef2e21970f55e70d6512.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,10 @@
-
 // swift-tools-version:5.8
+//
+//  Package.swift
+//  SHWireGuardKit
+//
+//
+
 import PackageDescription
 
 let package = Package(
@@ -9,24 +14,16 @@ let package = Package(
         .iOS(.v12)
     ],
     products: [
-        .library(
-            name: "SHWireGuardKit",
-            targets: ["SHWireGuardWrapper"]
-        )
+        .library(name: "SHWireGuardKit", targets: ["SHWireGuardKit"])
     ],
     targets: [
         .binaryTarget(
             name: "SHWireGuardKit",
             url: "https://github.com/safehousetech/safehouse-bodyguard-swift6-update/releases/download/1.0.0/Safehouse-bodyguard-swift6-update.zip",
             checksum: "708febe2a5b92147ac8ea791bded67d0db670a4e14d9a567f99ff951bf647155"
-        ),
-        .target(
-            name: "SHWireGuardWrapper",
-            dependencies: ["SHWireGuardKit"],
-            path: "Sources/SHWireGuardWrapper",
-            sources: ["."],
-            publicHeadersPath: "include"
         )
     ]
 )
+
 //https://github.com/safehousetech/SHWireGuardKit/releases/download/1.0.0/SHWireGuardKit.xcframework.zip
+
